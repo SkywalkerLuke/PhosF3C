98f03daa02ec2a1900e1e405d161d355bf5fcd91
Frankkkk233 <lyh3348198038@gmail.com>
2024-12-17 23:14:05 +0800

    Delete PhosF3C-main directory

--- PhosF3C-main/utils/model_utils.py
+++
@@ -1,7 +0,0 @@
-import torch      
-
-def get_esm_model(device):
-    model, alphabet = torch.hub.load("facebookresearch/esm:main", "esm2_t33_650M_UR50D")
-    batch_converter = alphabet.get_batch_converter()
-    model=model.to(device)
-    return batch_converter,model
